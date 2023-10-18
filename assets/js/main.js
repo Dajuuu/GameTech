60fd8fe5e2aa2f110243405368666d4e9bc94007
Dajuuu <40289712+Dajuuu@users.noreply.github.com>
2023-10-18 22:12:26 +0200

    5.12 Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,218 +1,163 @@
-/**
-* Template Name: Vlava - v2.2.1
-* Template URL: https://bootstrapmade.com/vlava-free-bootstrap-one-page-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
-  // Smooth scroll for the navigation menu and links with .scrollto classes
-  var scrolltoOffset = $('#header').outerHeight() - 15;
-  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      if (target.length) {
-        e.preventDefault();
+//--------------------------------------------    
+// Mobile Navigation
+// ***
+// This part was kept almost without any change as this was exactly what I needed
+//  - changed: some variable names
+    if ($('.nav-menu').length) {
+        var mobile_nav = $('.nav-menu').clone().prop(
+        {class: 'mobile-nav d-lg-none'});
 
-        var scrollto = target.offset().top - scrolltoOffset;
+        // Adding classes to the html
+        $('body').append(mobile_nav);
+        $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+        $('body').append('<div class="mobile-nav-overly"></div>');
 
-        if ($(this).attr("href") == '#header') {
-          scrollto = 0;
-        }
-
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
-
-//        if ($(this).parents('.nav-menu, .mobile-nav').length) {
-//          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-//          $(this).closest('li').addClass('active');
-//        }
-
-
-      }
-    }
-  });
-
-  // Activate smooth scroll on page load with hash links in the url
-//  $(document).ready(function() {
-//    if (window.location.hash) {
-//      var initial_nav = window.location.hash;
-//      if ($(initial_nav).length) {
-//        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
-//        $('html, body').animate({
-//          scrollTop: scrollto
-//        }, 1500, 'easeInOutExpo');
-//      }
-//    }
-//  });
-    
-//document.getElementsByClassName('carousel-indicators').style.backgroundColor = "black";
+        // Changes the icons when clicked
+        $(document).on('click', '.mobile-nav-toggle', function() {
+            $('body').toggleClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            $('.mobile-nav-overly').toggle();
+        });
         
-    
-    
-  // Mobile Navigation
-  if ($('.nav-menu').length) {
-    var $mobile_nav = $('.nav-menu').clone().prop({
-      class: 'mobile-nav d-lg-none'
-    });
-    $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
-    $('body').append('<div class="mobile-nav-overly"></div>');
-
-    $(document).on('click', '.mobile-nav-toggle', function(e) {
-      $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-      $('.mobile-nav-overly').toggle();
-    });
-
-    $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
-      e.preventDefault();
-      $(this).next().slideToggle(300);
-      $(this).parent().toggleClass('active');
-    });
-
-    $(document).click(function(e) {
-      var container = $(".mobile-nav, .mobile-nav-toggle");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          $('.mobile-nav-overly').fadeOut();
+        // When user clicks on the screen outside of the nav-bar box, the bar itself fades out
+        $(document).click(function(fadeOutBox) {
+            
+            var container = $(".mobile-nav, .mobile-nav-toggle");
+            
+            if (!container.is(fadeOutBox.target) && container.has(fadeOutBox.target).length === 0) {
+                if ($('body').hasClass('mobile-nav-active')) {
+                    $('body').removeClass('mobile-nav-active');
+                    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+                    $('.mobile-nav-overly').fadeOut();
+                }
+            }
+        });
+        
+    } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+        $(".mobile-nav, .mobile-nav-toggle").hide();
         }
-      }
-    });
-  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-    $(".mobile-nav, .mobile-nav-toggle").hide();
-  }
 
-  // Navigation active state on scroll
-  var nav_sections = $('section');
-  var main_nav = $('.nav-menu, .mobile-nav');
-
-  $(window).on('scroll', function() {
-    var cur_pos = $(this).scrollTop() + 200;
-
-    nav_sections.each(function() {
-      var top = $(this).offset().top,
-        bottom = top + $(this).outerHeight();
-
-//      if (cur_pos >= top && cur_pos <= bottom) {
-//        if (cur_pos <= bottom) {
-//          main_nav.find('li').removeClass('active');
-//        }
-//        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-//      }
-//      if (cur_pos < 300) {
-//        $(".nav-menu ul:first li:first").addClass('active');
-//      }
-    });
-  });
-
-  // Toggle .header-scrolled class to #header when page is scrolled
-    /* Added: making script to work only on the certain pages 
-    */
-$(window).scroll(function() {
-//    if($location.pathname=='../index.html' || $location.pathname=='../games.html' || $location.pathname=='../tech.html'){
+//--------------------------------------------
+// Changes the header size and positioning when scrolled
+// ***
+// Adds the .header-scrolled class to the #header and .mobile-nav-toggle-scrolled to the .mobile-nav-toggle  
+// .mobile-nav-toggle-scrolled changes the position of nav-bar icon when header shrinks
+//  - added: whole .mobile-nav-toggle behaviour
+    
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
             $('#header').addClass('header-scrolled');
             $('.mobile-nav-toggle').addClass('mobile-nav-toggle-scrolled');
         } else {
             $('#header').removeClass('header-scrolled');
             $('.mobile-nav-toggle').removeClass('mobile-nav-toggle-scrolled');
+        }
+    });
+
+//--------------------------------------------
+// This part makes the nav-bar menu more responsive - when the header becomes smaller, the navbar changes its position
+// - This code was written by myself
+
+    $(window).scroll(function() {
+
+        var bar_pos = $('.mobile-nav');
+
+        if ($(this).scrollTop() > 100) {
+            $(bar_pos).css("top", "70px");
+        } else {
+            $(bar_pos).css("top", "100px");
+        }
+    });       
+
+//--------------------------------------------
+// This is the scroll-to-top-button function, that appears in the 
+// bottom right corner of the screen after user is below fixed y coord.
+//  - changed: the y coordinate, animation time length
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 400) {
+            $('.scroll-to-top-button').fadeIn('slow');
+        } 
+        else {
+            $('.scroll-to-top-button').fadeOut('slow');
+        }
+    });
+
+    $('.scroll-to-top-button').click(function() {
+        $('html, body').animate({scrollTop: 0}, 1200, 'easeInOutExpo');
+        return false;
+    });
+    
+//--------------------------------------------
+// This is the scroll-to-top-button function, that appears in the 
+// bottom right corner of the screen after user is below fixed y coord.
+//  - changed: the y coordinate, animation time length
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 400) {
+            $('.scroll-to-top-button').fadeIn('slow');
+        } 
+        else {
+            $('.scroll-to-top-button').fadeOut('slow');
+        }
+    });
+
+    $('.scroll-to-top-button').click(function() {
+        $('html, body').animate({scrollTop: 0}, 1200, 'easeInOutExpo');
+        return false;
+    });
+//--------------------------------------------
+// Progress bar
+// ***
+// When user scrolls the progress bar indicates how long is to reach the bottom of the page
+// - This code was written by myself
+
+    $(window).scroll(function() {
+        
+        var yCoord = $(document).scrollTop();
+        var pageHeight = $(document).height() - $(window).height();
+        var line = yCoord / pageHeight * 100;
+
+        $(".progressBar").css("width", line + "%");
+        
+        if ($(window).scrollTop() > 100 && $(window).width() > 991) {
+            $(".progressBar").css("opacity" , "0.7"); 
+        } else {
+            $(".progressBar").css("opacity" , "0");
+        }
+    });
+    
+//--------------------------------------------    
+// This is the alert that pops up when clicking buttons and images on the shop page
+// - This code was written by myself
+
+    var btn_alert = $('.shop-image .description .btn , .shop-image-below');
+    
+    $(btn_alert).click(function() {
+        alert("Coming soon!");
+    });
+
+//--------------------------------------------
+// This is the "Load More" button behaviour on the shop page
+// - This section was copied and modified 
+// - https://codepen.io/Tigran_91/pen/JyaZPz 
+// - More in the report.html in Load more section
+
+    $(document).ready(function(){
+        $(".shop-image-below").slice(0, 4).show();
+
+        $("#loadMore").on("click", function(loadMoreBtn){
+
+            loadMoreBtn.preventDefault();
+            $(".shop-image-below:hidden").slice(0, 4).slideDown();
+
+            if($(".shop-image-below:hidden").length == 0) {
+                $("#loadMore").text("").addClass("noContent");
             }
-});
-if(location.pathname=='/index.html' || location.pathname=='/games.html' || location.pathname=='/tech.html'){
-if ($(window).scrollTop() > 100) {
-    $('#header').addClass('header-scrolled');
-    $('.mobile-nav-toggle').addClass('mobile-nav-toggle-scrolled');
-} 
-}
-    
-//      $(window).scroll(function() {
-//    if ($(this).scrollTop() > 100) {
-//      $('#header').addClass('header-scrolled');
-//    } else {
-//      $('#header').removeClass('header-scrolled');
-//    }
-//  });
-//
-//  if ($(window).scrollTop() > 100) {
-//    $('#header').addClass('header-scrolled');  
-//  } 
-    
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.mobile-nav-toggle').addClass('mobile-nav-toggle-scrolled');
-    } else {
-      $('.mobile-nav-toggle').removeClass('mobile-nav-toggle-scrolled');
-    }
-  });
-    
+        });
+    })
 
-/* 
-    This is the scroll-to-top-button function, that appears in the 
-    bottom right corner of the screen after user is below fixed y coord.
-    - This section was copied and modified
-    - https://codepen.io/matthewcain/pen/ZepbeR
-    - More in the report.html 
-*/   
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 400) {
-        $('.scroll-to-top-button').fadeIn('slow');
-    } 
-    else {
-        $('.scroll-to-top-button').fadeOut('slow');
-    }
-});
-
-$('.scroll-to-top-button').click(function() {
-    $('html, body').animate({scrollTop: 0}, 1200, 'easeInOutExpo');
-    return false;
-});
-    
-/* end of scroll to the top button*/
-    
-    
-// columns outline code
-//$(window).resize(function() {
-//    if($(window).width() < 550){
-//    var xlen = document.getElementsByClassName('column-outline-left column-outline-right').length;
-//    console.log(xlen);
-//    }
-//});
-    
-//$(document).ready(function(){
-//    $(".shop-image-below").slice(0, 4).show();
-//    $("#loadMore").on("click", function(e){
-//        e.preventDefault();
-//    $(".shop-image-below:hidden").slice(0, 4).slideDown(2000, 'linear');
-//        
-//    if($(".shop-image-below:hidden").length == 0) {
-//        $("#loadMore").text("").addClass("noContent");
-//    }
-//  });
-//})
-$(document).ready(function(){
-  $(".shop-image-below").slice(0, 4).show();
-  $("#loadMore").on("click", function(e){
-    e.preventDefault();
-    $(".shop-image-below:hidden").slice(0, 4).slideDown();
-    if($(".shop-image-below:hidden").length == 0) {
-      $("#loadMore").text("").addClass("noContent");
-    }
-  });
-})
-//    $(document).ready(function(){
-//    $(".shop-image-below").slice(0, 4).show();
-//    $("#loadMore").on("click", function(e){
-//        e.preventDefault();
-//    $(".shop-image-below:hidden").slice(0, 4).slideDown(1000);
-//    if($(".shop-image-below:hidden").length == 0) {
-//        $("#loadMore").text("").addClass("noContent");
-//    }
-//  });
-//})
-    
 })(jQuery);
